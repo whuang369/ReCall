d79674421064e9a203a38c578fe8c1e680eb969c
Wenyuan Huang <157926136+whuang369@users.noreply.github.com>
2025-10-22 18:19:53 -0500

    update scripts

--- install.sh
+++ install.sh
@@ -1,3 +1,10 @@
+mkdir miniconda3
+wget https://repo.anaconda.com/miniconda/Miniconda3-py310_24.5.0-0-Linux-x86_64.sh -O miniconda3/miniconda.sh
+bash miniconda3/miniconda.sh -b -u -p miniconda3
+
+# Initialize Conda
+source miniconda3/bin/activate
+
 conda create -n re-call python==3.10
 conda activate re-call
 
